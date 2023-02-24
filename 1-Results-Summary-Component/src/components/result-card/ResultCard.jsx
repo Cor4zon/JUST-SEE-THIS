3c0beb90611404557c33485c19910e325d457249
Victor Fedorov <vpfedorov25@gmail.com>
2023-02-24 17:24:52 +0400

    Finish ResultCard mobile design

--- 1-Results-Summary-Component/src/components/result-card/ResultCard.jsx
+++ 1-Results-Summary-Component/src/components/result-card/ResultCard.jsx
@@ -9,10 +9,10 @@ const ResultCard = () => {
                 <p className="result-score">76</p>
                 <p className="result-of-100">of 100</p>
             </div>
-            <p className="h3">`` Great</p>
+            <p className="h3">Great</p>
             <p className="result-info">
-                Your performance exceed 65% of the people conducting the test
-                here!
+                Your performance exceed 65% of <br /> the people conducting the
+                test here!
             </p>
         </div>
     )
