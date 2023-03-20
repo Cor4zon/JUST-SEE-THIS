60f0c620c9c9080ab6baa4acec02694f61dcd138
Victor Fedorov <vpfedorov25@gmail.com>
2023-03-20 23:00:53 +0400

    Update text on Cards

--- 4-Skilled-E-Learning-Landing-Page/src/App.jsx
+++ 4-Skilled-E-Learning-Landing-Page/src/App.jsx
@@ -40,20 +40,20 @@ function App() {
               description="Learn the latest animation techniques to create stunning motion design and captivate your audience."
             />
             <Card
-              title={'Animation'}
-              description="Learn the latest animation techniques to create stunning motion design and captivate your audience."
+              title={'Design'}
+              description="Create beautiful, usable interfaces to help shape the future of how the web looks."
             />
             <Card
-              title={'Animation'}
-              description="Learn the latest animation techniques to create stunning motion design and captivate your audience."
+              title={'Photography'}
+              description="Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos."
             />
             <Card
-              title={'Animation'}
-              description="Learn the latest animation techniques to create stunning motion design and captivate your audience."
+              title={'Crypto'}
+              description="All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course."
             />
             <Card
-              title={'Animation'}
-              description="Learn the latest animation techniques to create stunning motion design and captivate your audience."
+              title={'Business'}
+              description="A step-by-step playbook to help you start, scale, and sustain your business without outside investment."
             />
           </CardGrid>
         </Content>
