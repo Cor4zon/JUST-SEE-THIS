899ce7ac6f8d4656e919d036c26f192ddd7ef802
Victor Fedorov <vpfedorov25@gmail.com>
2023-04-18 20:46:30 +0400

    Added PlanCard component

--- 5-Multi-Step-Form/src/App.jsx
+++ 5-Multi-Step-Form/src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import { Button } from './components/button/Button'
+import PlanCard from './components/plan-card/PlanCard'
+
+import IconPro from './assets/images/icon-pro.svg';
 
 function App() {
 
   return (
     <div className="App">
-      <Button text="Play with me" />
+      {/* <Button text="Play with me" />
+      <Button text="Play with me" primary={true} /> */}
 
-      <Button text="Play with me" primary={true} />
+      <PlanCard icon={IconPro} price={15} name={'Pro'} />
     </div>
   )
 }
--- 5-Multi-Step-Form/src/components/plan-card/PlanCard.jsx
+++ 5-Multi-Step-Form/src/components/plan-card/PlanCard.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const PlanCard = () => {
+const PlanCard = ({color, icon, name, price}) => {
     return (
-        <div>
-            
-        </div>
+        <PlanCardContainer>
+            <PlanIcon $color={color} $icon={icon} />
+            <div>
+                <p>{name}</p>
+                <p>{price}</p>
+            </div>
+        </PlanCardContainer>
     );
 };
 
 export default PlanCard;
+
+const PlanCardContainer = styled.div`
+    background: #FFFFFF;
+    border: 1px solid #D6D9E6;
+    border-radius: 8px;
+
+    width: 295px;
+    height: 77px;
+`;
+
+const PlanIcon = styled.div`
+    background-image: url(${props => props.$icon});
+    width: 40px;
+    height: 40px;
+    border-radius: 100px;
+`;
