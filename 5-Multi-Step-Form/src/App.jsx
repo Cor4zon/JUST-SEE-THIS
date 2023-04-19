d93daf23e108cbbd0bdf7b8e602377bd713ceda1
Victor Fedorov <vpfedorov25@gmail.com>
2023-04-19 20:29:36 +0400

    Solved problem with prettier

--- 5-Multi-Step-Form/src/App.jsx
+++ 5-Multi-Step-Form/src/App.jsx
@@ -1,11 +1,10 @@
-import './App.css'
-import { Button } from './components/button/Button'
-import PlanCard from './components/plan-card/PlanCard'
+import './App.css';
+import { Button } from './components/button/Button';
+import PlanCard from './components/plan-card/PlanCard';
 
 import IconPro from './assets/images/icon-pro.svg';
 
 function App() {
-
   return (
     <div className="App">
       {/* <Button text="Play with me" />
@@ -13,7 +12,7 @@ function App() {
 
       <PlanCard icon={IconPro} price={15} name={'Pro'} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
